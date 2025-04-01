0ba3a6bdc76cd663c37df5951f9d5eeebe6fea8e
dantedevfr <dante.dev.fr@gmail.com>
2025-04-01 10:04:37 +0200

    updating menu

--- src/app/layouts/admin-layout/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/layouts/admin-layout/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AdminMenuComponent } from '../admin-menu/admin-menu.component';
 
 @Component({
   selector: 'app-admin-sidebar',
   standalone: true,
-  imports: [],
+  imports: [AdminMenuComponent],
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss'
 })
